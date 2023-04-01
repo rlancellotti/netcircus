b950fbe7ef1abf49f9e233f3b208095e6fb2e774
Riccardo Lancellotti <riccardo.lancellotti@unimore.it>
2023-04-01 11:47:43 +0200

    Revised scripts for image creation

---
+++ image/runimage.sh
@@ -0,0 +1,21 @@
+# run with:
+# linux ubd0=root.cow,root.ext4 con1=xterm umid=uml1
+# other parameters:
+# xterm=terminal emulator,title switch,exec switch es: xterm=gnome-terminal,-t,-x'
+# hostname=<hostname>
+SCRIPTDIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOTFS=${SCRIPTDIR}/root.ext4
+COWFILE=${SCRIPTDIR}/root.cow
+KERNEL=$(which linux)
+HOSTNAME=myuml
+
+if [ ${ROOTFS} -nt ${COWFILE} ]; then
+  echo "Removing old COW file: ${COWFILE}"
+  rm -f ${COWFILE}
+fi
+# RUN
+#${KERNEL} ubd0=${COWFILE},${ROOTFS} con1=xterm umid=uml1 hostname=test
+${KERNEL} ubd0=${COWFILE},${ROOTFS} con1=xterm umid=uml1
+
+
+
