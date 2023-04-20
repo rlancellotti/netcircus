5de26d57c3c9c096f454f3454817ccb4d64df310
Riccardo Lancellotti <riccardo.lancellotti@unimore.it>
2023-04-20 15:48:31 +0200

    New palete

--- gui/nc_palette.py
+++ gui/nc_palette.py
@@ -3,45 +3,66 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GdkPixbuf
 from component import ComponentModel
+from nc_canvas import NcCanvas
 
-(COLUMN_TEXT, COLUMN_PIXBUF, COLUMN_TYPE) = range(3)
+(ACTION_NONE, ACTION_MOVE, ACTION_CONNECT, ACTION_HOST, ACTION_SWITCH) = range(5)
 
 @Gtk.Template(filename="nc_palette.ui")
-class NcPalette(Gtk.IconView):
+class NcPalette(Gtk.Bin):
     __gtype_name__ = "NcPalette"
     #can define the named entries in the class
-    #entry = Gtk.Template.Child()
-    #button = Gtk.Template.Child()
+    btn_move=Gtk.Template.Child('btn_move')
+    btn_connect=Gtk.Template.Child('btn_connect')
+    btn_host=Gtk.Template.Child('btn_host')
+    btn_switch=Gtk.Template.Child('btn_switch')
+
     def __init__(self):
         super().__init__()
-        self.set_text_column(COLUMN_TEXT)
-        self.set_pixbuf_column(COLUMN_PIXBUF)
-        self.set_model(Gtk.ListStore(str, GdkPixbuf.Pixbuf, str))
-        self.add_item("Host", "network-server", ComponentModel.TYPE_HOST)
-        self.add_item("Switch", "gnome-modem", ComponentModel.TYPE_SWITCH)
-        self.add_item("Cable", "network-wired", ComponentModel.TYPE_UNKNOWN)
+        self.buttons={
+            ACTION_MOVE: self.btn_move,
+            ACTION_CONNECT: self.btn_connect,
+            ACTION_HOST: self.btn_host,
+            ACTION_SWITCH: self.btn_switch
+        }
+        self.canvas=None
+        self.action=ACTION_MOVE
+        self.set_action(self.action)
 
-        self.enable_model_drag_source(Gdk.ModifierType.BUTTON1_MASK, [], Gdk.DragAction.COPY)
-        self.connect("drag-begin", self.on_drag_begin)
-        self.connect("drag-data-get", self.on_drag_data_get)
+    def set_canvas(self, canvas: NcCanvas):
+        self.canvas=canvas
 
-    def add_item(self, text, icon_name, component):
-        pixbuf = Gtk.IconTheme.get_default().load_icon(icon_name, 48, 0)
-        self.get_model().append([text, pixbuf, component])
+    def get_action_from_widget(self, widget):
+        for action in self.buttons.keys():
+            if widget==self.buttons[action]:
+                print(f'identified wiget for action {action}')
+                return action
+
+    def is_no_action(self):
+        for act in self.buttons.keys():
+            if self.buttons[act].get_active():
+                return False
+        return True
+        
+
+    def set_action(self, action):
+        print(f'call to set_action({action}), old status is {self.action}')
+        self.action=action
+        if self.canvas is not None:
+            self.canvas.set_action(action)
+        self.buttons[action].set_active(True)
+        for act in self.buttons.keys():
+            if act != action:
+                self.buttons[act].set_active(False)
 
     @Gtk.Template.Callback()
-    def on_drag_data_get(self, widget, drag_context, data, info, time):
-        print(type(data))
-        selected_path = self.get_selected_items()[0]
-        selected_iter = self.get_model().get_iter(selected_path)
-        value = self.get_model().get_value(selected_iter, COLUMN_TYPE)
-        print(f'sending value {value}')
-        data.set_text(value, -1)
-
-    @Gtk.Template.Callback()
-    def on_drag_begin(self, widget, drag_context):
-        #print(f'call to on_drag_begin widget: {widget}, drag context: {drag_context}')
-        pass
+    def on_toggled(self, widget):
+        if widget.get_active():
+            action=self.get_action_from_widget(widget)
+            self.set_action(action)
+        else:
+            if self.is_no_action():
+                self.set_action(ACTION_MOVE)
+        print(f'toggle: action is: {self.action}')
 
 if __name__ == '__main__':
     window = Gtk.Window()
