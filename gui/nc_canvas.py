8d4afdaaabe94b7091faa620b599d7351e43f66f
Riccardo Lancellotti <riccardo.lancellotti@unimore.it>
2023-04-21 22:15:27 +0200

    Work on canvas: - Support for context menu - Hiliting of selected component

--- gui/nc_canvas.py
+++ gui/nc_canvas.py
@@ -21,6 +21,8 @@ class NcCanvas(Gtk.DrawingArea):
         self.current_link_start=None
         self.current_link_end=None
         self.action=None
+        self.selected_component=None
+        self.build_context_menu()
 
     def set_action(self, action):
         self.action=action
@@ -38,11 +40,19 @@ class NcCanvas(Gtk.DrawingArea):
         cx.line_to(*icon_center(l.b))
         cx.stroke()
     
-    def draw_dangling_link(self, cx: cairo.Context):
-        print('call to draw_dangling_link')
-        if self.current_link_start is not None and self.current_link_end is not None:
-            print(f'drawing danging link from {icon_center(self.network_model.get_component(self.current_link_start))} to {self.current_link_end}')
+    def draw_selection(self, cx):
+        if self.selected_component is not None:
+            print(f'drawing selection for component {self.selected_component}')
+            c=self.network_model.get_component(self.selected_component)
             cx.set_source_rgb(1, 0, 0)
+            cx.rectangle(c.x, c.y, c.width, c.height)
+            cx.stroke()
+
+    def draw_dangling_link(self, cx: cairo.Context):
+        #print('call to draw_dangling_link')
+        if self.current_link_start is not None and self.current_link_end is not None:
+            #print(f'drawing danging link from {icon_center(self.network_model.get_component(self.current_link_start))} to {self.current_link_end}')
+            cx.set_source_rgb(0.5, 0.5, 0.5)
             cx.move_to(*icon_center(self.network_model.get_component(self.current_link_start)))
             cx.line_to(*self.current_link_end)
             cx.stroke()
@@ -50,13 +60,12 @@ class NcCanvas(Gtk.DrawingArea):
     @Gtk.Template.Callback()
     def on_motion_notify_event(self, widget, event):
         if self.current_component is not None:
-            print('motion: updating component pos')
+            #print('motion: updating component pos')
             self.update_current_component_pos(event.x, event.y)
         if self.current_link_start is not None:
-            print('motion: updating dangling link')
+            #print('motion: updating dangling link')
             self.current_link_end=(event.x, event.y)
             self.queue_draw()
-
 
     @Gtk.Template.Callback()
     def on_draw(self, widget, cx):
@@ -66,6 +75,7 @@ class NcCanvas(Gtk.DrawingArea):
             self.draw_link(cx, l)
         for c in self.network_model.get_components():
             self.draw_component(cx, c)
+        self.draw_selection(cx)
 
     #@Gtk.Template.Callback()
     #def on_drag_data_received(self, widget, drag_context, x, y, data, info, time):
@@ -85,22 +95,45 @@ class NcCanvas(Gtk.DrawingArea):
         if event.button == 1:
             if self.action==ACTION_MOVE:
                 self.current_component=self.network_model.get_component_from_cords(event.x, event.y)
+                self.selected_component=self.network_model.get_component_from_cords(event.x, event.y)
             if self.action==ACTION_CONNECT:
                 self.current_link_start=self.network_model.get_component_from_cords(event.x, event.y)
                 print(f'connect from @({event.x}, {event.y}) -> {self.current_link_start}')
             if self.action==ACTION_HOST:
                 self.add_component(ComponentModel.TYPE_HOST, event.x, event.y)
+                self.selected_component=self.network_model.get_component_from_cords(event.x, event.y)
             if self.action==ACTION_SWITCH:
                 self.add_component(ComponentModel.TYPE_SWITCH, event.x, event.y)
-
+                self.selected_component=self.network_model.get_component_from_cords(event.x, event.y)
         if event.button == 3:
-            print(f'open context menu for component {self.current_component}')
+            #self.build_context_menu()
+            self.selected_component=self.network_model.get_component_from_cords(event.x, event.y)
+            print(f'pressedd button3: selected component is: {self.selected_component}')
+            self.cmenu.popup_at_pointer()
+        self.queue_draw()
+
 
     def update_current_component_pos(self, x, y):
         if self.current_component is not None and self.action==ACTION_MOVE:
             c=self.network_model.get_component(self.current_component)
             (c.x, c.y)=icon_coords_from_center(x, y, c.width, c.height)
             self.queue_draw()
+
+    def delete_component(self, widget):
+        print(f'deleting component {self.selected_component}')
+
+    def edit_component(self, widget):
+        print(f'editing component {self.selected_component}')
+
+    def build_context_menu(self):
+        self.cmenu = Gtk.Menu.new()
+        menucontent={'Delete': self.delete_component,
+                     'Edit': self.edit_component}
+        for mi in menucontent.keys():
+            item=Gtk.MenuItem.new_with_label(mi)
+            item.connect('activate', menucontent[mi])
+            self.cmenu.append(item)
+        self.cmenu.show_all()
 
     @Gtk.Template.Callback()
     def on_button_release(self, widget, event):
@@ -109,11 +142,11 @@ class NcCanvas(Gtk.DrawingArea):
             self.current_component=None
         if self.current_link_start is not None and self.action==ACTION_CONNECT:
             cb=self.network_model.get_component_from_cords(event.x, event.y)
-            if cb is not None:
+            if cb is not None and cb != self.current_link_start:
                 self.network_model.add_link(self.current_link_start, cb)
-                self.current_link_start=None
-                self.current_link_end=None
-                self.queue_draw()
+            self.queue_draw()
+            self.current_link_start=None
+            self.current_link_end=None
 
 def icon_coords_from_center(x, y, w, h):
     return (x-w/2, y-h/2)
