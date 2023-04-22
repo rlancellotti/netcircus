5e018a494a77b4be23cf318299a0b3758241f306
Riccardo Lancellotti <riccardo.lancellotti@unimore.it>
2023-04-22 18:52:26 +0200

    Support for deleting components

--- gui/nc_canvas.py
+++ gui/nc_canvas.py
@@ -24,38 +24,44 @@ class NcCanvas(Gtk.DrawingArea):
         self.selected_component=None
         self.build_context_menu()
 
+    def build_context_menu(self):
+        self.cmenu = Gtk.Menu.new()
+        menucontent={'Delete': self.delete_component,
+                     'Edit': self.edit_component}
+        for mi in menucontent.keys():
+            item=Gtk.MenuItem.new_with_label(mi)
+            item.connect('activate', menucontent[mi])
+            self.cmenu.append(item)
+        self.cmenu.show_all()
+
     def set_action(self, action):
         self.action=action
 
-    def draw_component(self, cx: cairo.Context, c: ComponentModel):
-        #print(f'drawing component @({c.x}, {c.y}, w={self.icons[c.type].get_width()}, h={self.icons[c.type].get_height()}), type={c.type}')
-        #print(self.icons[c.type])
-        cx.set_source_surface(self.icons[c.type], c.x, c.y)
-        cx.paint()
+    ############### Component+Link management ###############
 
-    def draw_link(self, cx: cairo.Context, l: LinkModel):
-        #print(f'drawing component @({c.x}, {c.y}, w={self.icons[c.type].get_width()}, h={self.icons[c.type].get_height()}), type={c.type}')
-        cx.set_source_rgb(0, 0, 0)
-        cx.move_to(*icon_center(l.a))
-        cx.line_to(*icon_center(l.b))
-        cx.stroke()
-    
-    def draw_selection(self, cx):
-        if self.selected_component is not None:
-            print(f'drawing selection for component {self.selected_component}')
-            c=self.network_model.get_component(self.selected_component)
-            cx.set_source_rgb(1, 0, 0)
-            cx.rectangle(c.x, c.y, c.width, c.height)
-            cx.stroke()
+    def add_component(self, comp_type, x, y):
+        if comp_type is not None and comp_type != ComponentModel.TYPE_UNKNOWN:
+            w=self.icons[comp_type].get_width()
+            h=self.icons[comp_type].get_height()
+            self.network_model.add_component(comp_type, *icon_coords_from_center(x, y, w, h), w, h)
+            self.queue_draw()
 
-    def draw_dangling_link(self, cx: cairo.Context):
-        #print('call to draw_dangling_link')
-        if self.current_link_start is not None and self.current_link_end is not None:
-            #print(f'drawing danging link from {icon_center(self.network_model.get_component(self.current_link_start))} to {self.current_link_end}')
-            cx.set_source_rgb(0.5, 0.5, 0.5)
-            cx.move_to(*icon_center(self.network_model.get_component(self.current_link_start)))
-            cx.line_to(*self.current_link_end)
-            cx.stroke()
+    def delete_component(self, widget):
+        print(f'deleting component {self.selected_component}')
+        self.network_model.delete_component(self.selected_component)
+        self.selected_component=None
+        self.queue_draw()
+
+    def edit_component(self, widget):
+        print(f'editing component {self.selected_component}')
+
+    def update_current_component_pos(self, x, y):
+        if self.current_component is not None and self.action==ACTION_MOVE:
+            c=self.network_model.get_component(self.current_component)
+            (c.x, c.y)=icon_coords_from_center(x, y, c.width, c.height)
+            self.queue_draw()
+
+    ############### Callbacks ###############
 
     @Gtk.Template.Callback()
     def on_motion_notify_event(self, widget, event):
@@ -67,29 +73,12 @@ class NcCanvas(Gtk.DrawingArea):
             self.current_link_end=(event.x, event.y)
             self.queue_draw()
 
-    @Gtk.Template.Callback()
-    def on_draw(self, widget, cx):
-        #print(f'call to draw method widget: {widget}, cairo context: {cr}')
-        self.draw_dangling_link(cx)
-        for l in self.network_model.get_links():
-            self.draw_link(cx, l)
-        for c in self.network_model.get_components():
-            self.draw_component(cx, c)
-        self.draw_selection(cx)
-
     #@Gtk.Template.Callback()
     #def on_drag_data_received(self, widget, drag_context, x, y, data, info, time):
     #    print(f'call to on_drag_data_get method widget: {widget}, drag context: {drag_context}, x: {x}, y: {y}, {data.get_text()}')
     #    comp_type=data.get_text()
     #    self.add_component(comp_type, x, y)
-    
-    def add_component(self, comp_type, x, y):
-        if comp_type is not None and comp_type != ComponentModel.TYPE_UNKNOWN:
-            w=self.icons[comp_type].get_width()
-            h=self.icons[comp_type].get_height()
-            self.network_model.add_component(comp_type, *icon_coords_from_center(x, y, w, h), w, h)
-            self.queue_draw()
-    
+        
     @Gtk.Template.Callback()
     def on_button_press(self, widget, event):
         if event.button == 1:
@@ -112,29 +101,6 @@ class NcCanvas(Gtk.DrawingArea):
             self.cmenu.popup_at_pointer()
         self.queue_draw()
 
-
-    def update_current_component_pos(self, x, y):
-        if self.current_component is not None and self.action==ACTION_MOVE:
-            c=self.network_model.get_component(self.current_component)
-            (c.x, c.y)=icon_coords_from_center(x, y, c.width, c.height)
-            self.queue_draw()
-
-    def delete_component(self, widget):
-        print(f'deleting component {self.selected_component}')
-
-    def edit_component(self, widget):
-        print(f'editing component {self.selected_component}')
-
-    def build_context_menu(self):
-        self.cmenu = Gtk.Menu.new()
-        menucontent={'Delete': self.delete_component,
-                     'Edit': self.edit_component}
-        for mi in menucontent.keys():
-            item=Gtk.MenuItem.new_with_label(mi)
-            item.connect('activate', menucontent[mi])
-            self.cmenu.append(item)
-        self.cmenu.show_all()
-
     @Gtk.Template.Callback()
     def on_button_release(self, widget, event):
         if self.current_component is not None and self.action==ACTION_MOVE:
@@ -147,6 +113,48 @@ class NcCanvas(Gtk.DrawingArea):
             self.queue_draw()
             self.current_link_start=None
             self.current_link_end=None
+
+    @Gtk.Template.Callback()
+    def on_draw(self, widget, cx):
+        #print(f'call to draw method widget: {widget}, cairo context: {cr}')
+        self.draw_dangling_link(cx)
+        for l in self.network_model.get_links():
+            self.draw_link(cx, l)
+        for c in self.network_model.get_components():
+            self.draw_component(cx, c)
+        self.draw_selection(cx)
+
+    ############### Canvas draw ###############
+
+    def draw_component(self, cx: cairo.Context, c: ComponentModel):
+        #print(f'drawing component @({c.x}, {c.y}, w={self.icons[c.type].get_width()}, h={self.icons[c.type].get_height()}), type={c.type}')
+        #print(self.icons[c.type])
+        cx.set_source_surface(self.icons[c.type], c.x, c.y)
+        cx.paint()
+
+    def draw_link(self, cx: cairo.Context, l: LinkModel):
+        #print(f'drawing component @({c.x}, {c.y}, w={self.icons[c.type].get_width()}, h={self.icons[c.type].get_height()}), type={c.type}')
+        cx.set_source_rgb(0, 0, 0)
+        cx.move_to(*icon_center(l.a))
+        cx.line_to(*icon_center(l.b))
+        cx.stroke()
+    
+    def draw_selection(self, cx):
+        if self.selected_component is not None:
+            #print(f'drawing selection for component {self.selected_component}')
+            c=self.network_model.get_component(self.selected_component)
+            cx.set_source_rgb(1, 0, 0)
+            cx.rectangle(c.x, c.y, c.width, c.height)
+            cx.stroke()
+
+    def draw_dangling_link(self, cx: cairo.Context):
+        #print('call to draw_dangling_link')
+        if self.current_link_start is not None and self.current_link_end is not None:
+            #print(f'drawing danging link from {icon_center(self.network_model.get_component(self.current_link_start))} to {self.current_link_end}')
+            cx.set_source_rgb(0.5, 0.5, 0.5)
+            cx.move_to(*icon_center(self.network_model.get_component(self.current_link_start)))
+            cx.line_to(*self.current_link_end)
+            cx.stroke()
 
 def icon_coords_from_center(x, y, w, h):
     return (x-w/2, y-h/2)
