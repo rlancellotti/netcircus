94de5ed16805812b0b347bc47c5cb1f9149bd49e
LorenzoCorradi <lorenzocorradi01@gmail.com>
2023-07-17 18:17:33 +0200

    Add dialog for save/load projects

--- gui/nc_main_wnd.py
+++ gui/nc_main_wnd.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import subprocess
 import sys
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -18,6 +19,8 @@ class NcMainWnd(Gtk.Window):
     btn_run=Gtk.Template.Child('btn_run')
     btn_stop=Gtk.Template.Child('btn_stop')
     btn_halt=Gtk.Template.Child('btn_halt')
+    btn_save=Gtk.Template.Child('btn_save')
+    btn_load=Gtk.Template.Child('btn_load')
     def __init__(self):
         super().__init__(title="NetCircus")
         #print(self.palette, self.canvas)
@@ -31,10 +34,52 @@ class NcMainWnd(Gtk.Window):
             self.canvas.stop_network()
         if widget == self.btn_halt:
             self.canvas.halt_network()
+        if widget == self.btn_load:
+            dialog = Gtk.FileChooserDialog(
+            title="Please choose a folder",
+            parent=self,
+            action=Gtk.FileChooserAction.SELECT_FOLDER,
+            )
+            dialog.add_buttons(
+            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
+            )
+            dialog.set_default_size(800, 400)
+
+            response = dialog.run()
+            if response == Gtk.ResponseType.OK:
+                print("Select clicked")
+                print("Folder selected: " + dialog.get_filename())
+            elif response == Gtk.ResponseType.CANCEL:
+                print("Cancel clicked")
+
+            dialog.destroy()
+
+        
+        if widget == self.btn_save:
+            dialog = Gtk.FileChooserDialog(
+            title="Please choose a folder",
+            parent=self,
+            action=Gtk.FileChooserAction.SELECT_FOLDER,
+            )
+            dialog.add_buttons(
+            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
+            )
+            dialog.set_default_size(800, 400)
+
+            response = dialog.run()
+            if response == Gtk.ResponseType.OK:
+                print("Select clicked")
+                print("Folder selected: " + dialog.get_filename())
+            elif response == Gtk.ResponseType.CANCEL:
+                print("Cancel clicked")
+
+            dialog.destroy()
 
     @Gtk.Template.Callback()
     def on_menu_toggled(self, widget):
         print('must toggle properties menu')
+
+
 
 win = NcMainWnd()
 win.connect("destroy", Gtk.main_quit)
